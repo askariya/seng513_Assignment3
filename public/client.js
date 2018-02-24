1cd6edc4ae7cdac8f6785726c90a8bffa2792fdf
askariya <arunaluk@yahoo.ca>
2018-02-23 19:25:41 -0700

    Adding code for timestamp and adding colours to the message/user displays

--- public/client.js
+++ public/client.js
@@ -2,11 +2,15 @@
 $(function() {
     var socket = io();
     $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
-	return false;
+        socket.emit('chat', $('#m').val());
+        $('#m').val('');
+        return false;
     });
+
     socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+        var dt = new Date();
+        var utcDate = dt.toLocaleTimeString(); //calculate timestamp and format
+        var separator = '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0';
+        $('#messages').append($('<li>').text(utcDate + separator + msg));
     });
 });
